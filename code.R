78d8c1e44521e96c776746ebc19636c8e02f0810
Nhat Hoang <hoangphamkhanhnhat@gmail.com>
2018-03-21 20:46:22 +1100

    Create Dataframe Corpus from a normal Dataframe

--- code.R
+++ code.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(tm)
+library(lubridate)
+library(qdap)
 
 tweets = read_csv('coffee.csv')
 
@@ -10,3 +12,15 @@ coffee_source = VectorSource(coffee_tweets)
 
 #Create a volatile corpus
 coffee_corpus = VCorpus(coffee_source)
+
+#Create an example dataframe of text from tweets
+example_text = tweets[1:10, c(1, 2, 5)]
+example_text$created = mdy_hm(example_text$created)
+colnames(example_text) = c('doc_id', 'text', 'date_time')
+
+#Create a dataframe source
+df_source = DataframeSource(example_text)
+
+#Create a VCorpus from df_source
+df_corpus = VCorpus(df_source)
+
